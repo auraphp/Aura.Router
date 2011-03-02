6a7963fb98d9d7b0aff5201449c9d61241aa7995
Paul M. Jones <pmjones88@gmail.com>
2011-03-01 20:36:15 -0600

    add an all-files src.php file

---
+++ scripts/instance.php
@@ -0,0 +1,4 @@
+<?php
+namespace aura\router;
+require_once dirname(__DIR__) . '/src.php';
+return new Map(new RouteFactory);
---
+++ src.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . '/src/Exception.php';
+require __DIR__ . '/src/Map.php';
+require __DIR__ . '/src/Route.php';
+require __DIR__ . '/src/RouteFactory.php';
