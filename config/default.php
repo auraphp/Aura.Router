759bf4ae36be04444233ff58dfe5d08b67693639
Paul M. Jones <pmjones88@gmail.com>
2011-10-03 10:31:44 -0500

    config now adds to autoloader

--- config/default.php
+++ config/default.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * Package prefix for autoloader.
+ */
+$loader->addPrefix('Aura\Router\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
+
+/**
  * Dependency services.
  */
 $di->params['Aura\Router\Map']['route_factory'] = $di->lazyNew('Aura\Router\RouteFactory');
---
+++ config/test.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Package prefix for autoloader.
+ */
+$loader->addPrefix('Aura\Router\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'tests');
