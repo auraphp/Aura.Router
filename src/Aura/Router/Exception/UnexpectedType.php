717beefa366861ad644c04ec7c1c6c72b8776a5e
Paul M. Jones <pmjones88@gmail.com>
2013-01-04 09:46:33 -0600

    add specific exception types

---
+++ src/Aura/Router/Exception/UnexpectedType.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ * This file is part of the Aura Project for PHP.
+ * 
+ * @package Aura.Router
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
+namespace Aura\Router\Exception;
+
+use Aura\Router\Exception;
+
+/**
+ * 
+ * A different variable type was expected.
+ * 
+ * @package Aura.Router
+ * 
+ */
+class UnexpectedType extends Exception
+{
+}
