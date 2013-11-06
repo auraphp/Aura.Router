31d7dde0408ce795d5e095aec5c20792faba855d
Paul M. Jones <pmjones88@gmail.com>
2013-11-06 09:55:32 -0600

    add exception

---
+++ src/Exception/MalformedSubpattern.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ * This file is part of the Aura for PHP.
+ * 
+ * @package Aura.Router
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
+namespace Aura\Router\Exception;
+
+use Aura\Router\Exception;
+
+/**
+ * 
+ * Param subpattern definitions must start with an opening parenthesis.
+ * 
+ * @package Aura.Router
+ * 
+ */
+class MalformedSubpattern extends Exception
+{
+}
