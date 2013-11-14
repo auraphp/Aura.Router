db0ca396e607ca457d21a49018bea2e34c521ba2
Paul M. Jones <pmjones88@gmail.com>
2013-11-14 17:15:04 -0600

    new config

--- config/default.php
+++ config/default.php
@@ -1,7 +1,14 @@
 <?php
 /**
+ * Aura\Router\RouteCollection
+ */
+$di->params['Aura\Router\RouteCollection'] = array(
+    'route_factory' => $di->lazyNew('Aura\Router\RouteFactory'),
+);
+
+/**
  * Aura\Router\Router
  */
 $di->params['Aura\Router\Router'] = array(
-    'route_factory' => $di->lazyNew('Aura\Router\RouteFactory'),
+    'routes' => $di->lazyNew('Aura\Router\RouteCollection'),
 );
