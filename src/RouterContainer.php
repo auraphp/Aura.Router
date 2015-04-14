aabca7b8d03f56b04ff11ddbc10ad047425ca6ad
Paul M. Jones <pmjones88@gmail.com>
2015-04-13 21:29:57 -0500

    rename RouterFactory to RouterContainer

---
+++ src/RouterContainer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *
+ * This file is part of the Aura for PHP.
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ */
+namespace Aura\Router;
+
+/**
+ *
+ * A factory to create a router.
+ *
+ * @package Aura.Router
+ *
+ */
+class RouterContainer
+{
+    protected $map;
+
+    public function getMap()
+    {
+        if (! $this->map) {
+            $this->map = new Map($this->newRouteFactory());
+        }
+    }
+
+    public function newRouteFactory()
+    {
+        return new RouteFactory();
+    }
+
+    public function newMatcher()
+    {
+        return new Matcher($this->getMap());
+    }
+
+    public function newGenerator()
+    {
+        return new Generator($this->getMap());
+    }
+}
