c6de03549f17ed8eef7ef680fac0a222435925a7
Paul M. Jones <pmjones88@gmail.com>
2015-04-16 15:22:11 -0500

    add ImmutableProperty exception

---
+++ src/Exception/ImmutableProperty.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * This file is part of Aura for PHP.
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ */
+namespace Aura\Router\Exception;
+
+use Aura\Router\Exception;
+
+/**
+ *
+ * Tried to change a property value once set.
+ *
+ * @package Aura.Router
+ *
+ */
+class ImmutableProperty extends Exception
+{
+}
