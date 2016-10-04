7e7e7f4b61a3bfcf6ddf892372f227e230162b4e
David Lundgren <dlundgren@syberisle.net>
2016-10-04 17:12:53 -0500

    Change license to MIT to match the project

--- src/Helper/Url.php
+++ src/Helper/Url.php
@@ -3,7 +3,7 @@
  *
  * This file is part of Aura for PHP.
  *
- * @license http://opensource.org/licenses/bsd-license.php BSD
+ * @license http://opensource.org/licenses/mit-license.php MIT
  *
  */
 namespace Aura\Router\Helper;
