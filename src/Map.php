b48e74064b36817e97a3308d00f20e839f90937f
Andreas Sundqvist <andreas@forme.se>
2017-07-05 20:27:18 +0200

    Add doc block information to Map class to handle proxy call to Route

--- src/Map.php
+++ src/Map.php
@@ -17,6 +17,40 @@ use IteratorAggregate;
  *
  * @package Aura.Router
  *
+ * @method Route accepts(string|array $accepts)
+ *
+ * @method Route allows(string|array $allows)
+ *
+ * @method Route attributes(array $attributes)
+ *
+ * @method Route auth(mixed $auth)
+ *
+ * @method Route defaults(array $defaults)
+ *
+ * @method Route extras(array $extras)
+ *
+ * @method Route failedRule(mixed $failedRule)
+ *
+ * @method Route handler(mixed $handler)
+ *
+ * @method Route host(mixed $host)
+ *
+ * @method Route isRoutable(bool $isRoutable = true)
+ *
+ * @method Route namePrefix(string $namePrefix)
+ *
+ * @method Route path(string $path)
+ *
+ * @method Route pathPrefix(string $pathPrefix)
+ *
+ * @method Route secure(bool|null $secure = true)
+ *
+ * @method Route special(callable|null $host)
+ *
+ * @method Route tokens(array $tokens)
+ *
+ * @method Route wildcard(string $wildcard)
+ *
  */
 class Map implements IteratorAggregate
 {
